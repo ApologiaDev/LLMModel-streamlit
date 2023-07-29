3587ff582eea2ad353b8e3eef039b09d7ef6270b
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-07-29 14:50:42 -0400

    working streamlit

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ uploaded_model_file = st.file_uploader('Choose a model file (.zip)')
 if uploaded_model_file is not None:
     # initialize context
     with ZipFile(uploaded_model_file, 'r') as zip_ref:
-        os.makedirs('model')
+        if not os.path.isdir('model'):
+            os.makedirs('model')
         zip_ref.extractall('model')
     config = json.load(open(os.path.join('model', 'config.json'), 'r'))
     llm = get_llm_model(config)
@@ -36,8 +37,13 @@ if uploaded_model_file is not None:
             return_source_documents=True
         )
 
-    # input
-    input_question_text = st.text_area('Question')
-    if st.button('Ask!'):
-        results = qa({'query': input_question_text})
-        st.write(results['text'])
+# input
+input_question_text = st.text_area('Question')
+
+# get results
+if st.button('Ask!'):
+    if uploaded_model_file is None:
+        st.write('Please upload a model!')
+    else:
+        response = qa({'query': input_question_text})
+        st.write(response['result'])
